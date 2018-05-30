fc8801474a3a6c59f558240ec0990d07a5ba4d10
Zane P <ztpence@gmail.com>
2018-05-29 19:49:09 -0700

    added models code

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,21 @@
 // require config
+var orm = require('../config/orm.js');
+
+var burger = {
+    selectAll: function(cb) {
+        orm.selectAll('burgers', function(response){
+            cb(response);
+        });
+
+    },
+
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne('burgers', cols, vals, cb);
+    },
+
+    updateOne: function(condition, cb) {
+        orm.updateOne('burgers', {devoured: true}, condition, cb);
+    }
+};
+
+module.exports = burger;
